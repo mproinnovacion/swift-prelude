425ba29221df35e463d7f72bcdae0f34d5b4af1a
rgarciah <ruben.garcia@tyris-software.com>
2023-11-03 14:04:28 +0100

    SwiftDocs added

--- Sources/Prelude/AnyPublisher.swift
+++ Sources/Prelude/AnyPublisher.swift
@@ -6,7 +6,20 @@
 //
 
 import Combine
-
+/**
+  This function is to guarantee compatibility between old parts of Combine and new parts of AsyncAwait, with it you can convert easily from a async await request to a combine
+ 
+ AnyPublisher.asyncToPublisher {
+    return try await fetchDataFromNetwork()
+ }
+ .sink { _ in
+ 
+ }
+ receiveValue: { data in
+    print("Data received: \(data)")
+ }
+ store(in: &cancellables)
+ */
 public extension AnyPublisher {
      static func asyncToPublisher<T>(
         _ asyncCall: @escaping @Sendable () async throws -> T
